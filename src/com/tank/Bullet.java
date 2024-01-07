c1a24be0e612d4e5a2972f34c9251ff56e0458e6
zhangwanying-hik <2514261645@qq.com>
2024-01-07 17:30:39 +0800

    从坦克中心打出子弹

--- src/com/tank/Bullet.java
+++ src/com/tank/Bullet.java
@@ -4,7 +4,8 @@ import java.awt.*;
 
 public class Bullet {
     private static final int SPEED = 10;
-    private static int WIDTH = 30,HEIGHT = 30;
+    public static int WIDTH = ResourceMgr.bulletD.getWidth();
+    public static int HEIGHT = ResourceMgr.bulletD.getHeight();
 
     private int x,y;
     private Dir dir;
--- src/com/tank/Tank.java
+++ src/com/tank/Tank.java
@@ -7,6 +7,9 @@ public class Tank {
     private Dir dir = Dir.DOWN;
     private static final int SPEED=5;
 
+    public static int WIDTH = ResourceMgr.tankD.getWidth();
+    public static int HEIGHT = ResourceMgr.tankD.getHeight();
+
     private boolean moving = false;
 
     private TankFrame tf = null;
@@ -87,7 +90,10 @@ public class Tank {
     }
 
     public void fire(){
+        //从坦克的中心发出子弹
+        int bX = this.x + Tank.WIDTH/2 -Bullet.WIDTH/2;
+        int by = this.y + Tank.HEIGHT/2 - Bullet.HEIGHT/2;
 
-        tf.bullets.add(new Bullet(this.x,this.y,this.dir,this.tf));
+        tf.bullets.add(new Bullet(bX,by,this.dir,this.tf));
     }
 }
